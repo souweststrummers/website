97ed0fecdbbc65d74d02d7b275a9384d8fd847a9
Daniel Sherratt <84758903+danielsherratt@users.noreply.github.com>
2026-05-01 23:06:24 +1200

    Update songs-data.js

--- assets/songs-data.js
+++ assets/songs-data.js
@@ -1,10 +1,9 @@
-const songsData = [
 /*
 * Watch for Comma's, the last item should not have one
 * "chords": ["Em", "B7", "E7", "F#", "D7"], //These will be non standard chords
 *    "speed": 17,
 *    "zoom": 3.25,  //325%
-*    "routes": [
+*    "routes": 'squrebracket'
 *    {
 *      "at": 0.40,      // Trigger location 
 *      "goto": 0.20,    // Jump destination
@@ -12,9 +11,12 @@ const songsData = [
 *      "mode": "pause", // "scroll" or "pause"
 *      "then": 0.65     // 0.60, "continue", or "stop"
 *    }
-*   ]
+*   'squrebracket'
 */
-  
+
+
+
+const songsData = [
   {
     "id": "song-1",
     "number": "1.",
