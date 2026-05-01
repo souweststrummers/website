c362c2026850a78ed751f4741125de9df4339e25
Daniel Sherratt <84758903+danielsherratt@users.noreply.github.com>
2026-05-01 22:07:31 +1200

    Update songs-data.js

--- assets/songs-data.js
+++ assets/songs-data.js
@@ -2261,8 +2261,6 @@ const songsData = [
     "dataName": "251. Those Were the Days | Test | Test",
     "chords": ["Em", "B7", "E7", "F#", "D7"],
     "speed": 17,
-    "chorusPage": 1,
-    "chorusY": 0.20,
     "zoom": 3.25
   },
   {
